7418e4e04bb56bbfdcd0292bbb0540f483f38e16
Jack Varble <jackvarble@gmail.com>
2022-06-22 15:30:33 -0500

    style dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useReducer } from "react";
+import { useState, useEffect, useReducer } from "react";
 import DigitBtn from "./components/DigitBtn";
 import OperatorBtn from "./components/OperatorBtn";
+import { IoMdSunny, IoMdMoon } from "react-icons/io";
 import "./App.scss";
 
 export const ACTIONS = {
@@ -18,6 +19,7 @@ const reducer = (state, { type, payload }) => {
         return {
           ...state,
           currentEntry: payload.digit,
+          allClear: false,
           overwrite: false,
         };
       }
@@ -28,6 +30,7 @@ const reducer = (state, { type, payload }) => {
       return {
         ...state,
         currentEntry: `${state.currentEntry || ""}${payload.digit}`,
+        allClear: false,
       };
     case ACTIONS.CHOOSE_OPERATION:
       if (state.currentEntry == null && state.previousEntry == null) {
@@ -37,6 +40,7 @@ const reducer = (state, { type, payload }) => {
         return {
           ...state,
           operation: payload.operation,
+          allClear: true,
         };
       }
       if (state.previousEntry == null) {
@@ -45,6 +49,7 @@ const reducer = (state, { type, payload }) => {
           operation: payload.operation,
           previousEntry: state.currentEntry,
           currentEntry: null,
+          allClear: false,
         };
       }
       return {
@@ -52,6 +57,7 @@ const reducer = (state, { type, payload }) => {
         previousEntry: evaluate(state),
         operation: payload.operation,
         currentEntry: null,
+        allClear: false,
       };
     case ACTIONS.EQUALS:
       if (
@@ -64,11 +70,21 @@ const reducer = (state, { type, payload }) => {
       return {
         ...state,
         overwrite: true,
+        allClear: true,
         currentEntry: evaluate(state),
         previousEntry: null,
         operation: null,
       };
     case ACTIONS.CLEAR:
+      if (!state.allClear) {
+        return {
+          ...state,
+          currentEntry: null,
+          previousEntry: state.previousEntry,
+          operation: state.operation,
+          allClear: true,
+        };
+      }
       return {};
     case ACTIONS.DELETE:
       if (state.overwrite) {
@@ -76,6 +92,7 @@ const reducer = (state, { type, payload }) => {
           ...state,
           currentEntry: null,
           overwrite: false,
+          allClear: true,
         };
       }
       if (state.currentEntry == null) return state;
@@ -131,43 +148,71 @@ const App = () => {
     {}
   );
 
+  const [drkMode, setDrkMode] = useState(false);
+
+  const drkModeHandler = () => {
+    setDrkMode(true);
+  };
+
+  const lightModeHandler = () => {
+    setDrkMode(false);
+  };
+
+  const colorChange = drkMode ? "everything-dark" : "everything-light";
+
   return (
-    <main className="container">
-      <div className="output">
-        <div className="previousEntry">
-          {formatInteger(previousEntry)} {operation}
+    <div
+      className={drkMode ? "bodyDark" : "bodyLight"}
+      onKeyDown={lightModeHandler}
+    >
+      <main className={colorChange}>
+        <div className="container">
+          <div className="output">
+            <div className="previousEntry">
+              {formatInteger(previousEntry)} {operation}
+            </div>
+            <div className="currentEntry">{formatInteger(currentEntry)}</div>
+          </div>
+          <button
+            className="spanTwo color"
+            onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+          >
+            {currentEntry != null && previousEntry != null ? "C" : "AC"}
+          </button>
+          <button
+            onClick={() => dispatch({ type: ACTIONS.DELETE })}
+            className="color"
+          >
+            DEL
+          </button>
+          <OperatorBtn operation="÷" dispatch={dispatch} className="color" />
+          <DigitBtn digit="1" dispatch={dispatch} />
+          <DigitBtn digit="2" dispatch={dispatch} />
+          <DigitBtn digit="3" dispatch={dispatch} />
+          <OperatorBtn operation="*" dispatch={dispatch} className="color" />
+          <DigitBtn digit="4" dispatch={dispatch} />
+          <DigitBtn digit="5" dispatch={dispatch} />
+          <DigitBtn digit="6" dispatch={dispatch} />
+          <OperatorBtn operation="+" dispatch={dispatch} className="color" />
+          <DigitBtn digit="7" dispatch={dispatch} />
+          <DigitBtn digit="8" dispatch={dispatch} />
+          <DigitBtn digit="9" dispatch={dispatch} />
+          <OperatorBtn operation="-" dispatch={dispatch} className="color" />
+          <DigitBtn digit="." dispatch={dispatch} />
+          <DigitBtn digit="0" dispatch={dispatch} />
+          <button
+            className="spanTwo color"
+            onClick={() => dispatch({ type: ACTIONS.EQUALS })}
+          >
+            =
+          </button>
         </div>
-        <div className="currentEntry">{formatInteger(currentEntry)}</div>
-      </div>
-      <button
-        className="spanTwo"
-        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-      >
-        AC
-      </button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE })}>DEL</button>
-      <OperatorBtn operation="÷" dispatch={dispatch} />
-      <DigitBtn digit="1" dispatch={dispatch} />
-      <DigitBtn digit="2" dispatch={dispatch} />
-      <DigitBtn digit="3" dispatch={dispatch} />
-      <OperatorBtn operation="*" dispatch={dispatch} />
-      <DigitBtn digit="4" dispatch={dispatch} />
-      <DigitBtn digit="5" dispatch={dispatch} />
-      <DigitBtn digit="6" dispatch={dispatch} />
-      <OperatorBtn operation="+" dispatch={dispatch} />
-      <DigitBtn digit="7" dispatch={dispatch} />
-      <DigitBtn digit="8" dispatch={dispatch} />
-      <DigitBtn digit="9" dispatch={dispatch} />
-      <OperatorBtn operation="-" dispatch={dispatch} />
-      <DigitBtn digit="." dispatch={dispatch} />
-      <DigitBtn digit="0" dispatch={dispatch} />
-      <button
-        className="spanTwo"
-        onClick={() => dispatch({ type: ACTIONS.EQUALS })}
-      >
-        =
-      </button>
-    </main>
+        <div className="bottomRow">
+          <IoMdSunny onClick={lightModeHandler} className="drk_lt" />
+          <IoMdMoon onClick={drkModeHandler} className="drk_lt" />
+        </div>
+      </main>
+    </div>
   );
 };
 
--- src/components/OperatorBtn.js
+++ src/components/OperatorBtn.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { ACTIONS } from "../App";
 
-const OperatorBtn = ({ dispatch, operation }) => {
+const OperatorBtn = ({ dispatch, operation, className }) => {
   return (
     <button
       onClick={() =>
         dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
       }
+      className={className}
     >
       {operation}
     </button>
